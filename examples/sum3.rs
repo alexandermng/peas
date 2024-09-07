7df3af4b32d480ed040b6acadf1b7a6472aa251a
Alexander Ng <an@asu.edu>
2024-09-06 19:35:52 -0700

    BIG BRAIN FITNESS FUNCTION

--- examples/sum3.rs
+++ examples/sum3.rs
@@ -21,6 +21,70 @@ struct Sum3 {
 	tests: Vec<((i32, i32, i32), i32)>, // input-output pairs
 }
 
+impl Sum3 {
+	pub fn new(num: usize) -> Self {
+		const PARTIAL1_TESTS_RATE: f64 = 0.1;
+		const PARTIAL2_TESTS_RATE: f64 = 0.2;
+		let mut tests = Vec::with_capacity(num);
+		let partial1_tests_num = (num as f64 * PARTIAL1_TESTS_RATE) as usize;
+		let partial2_tests_num = (num as f64 * PARTIAL2_TESTS_RATE) as usize;
+		let dist = Uniform::new(-256, 256);
+		let rng = &mut thread_rng();
+
+		// partial a
+		tests.extend(
+			(0..partial1_tests_num)
+				.map(|_| (dist.sample(rng), 0, 0))
+				.map(|i| (i, (i.0))),
+		);
+
+		// partial b
+		tests.extend(
+			(0..partial1_tests_num)
+				.map(|_| (0, dist.sample(rng), 0))
+				.map(|i| (i, (i.1))),
+		);
+
+		// partial c
+		tests.extend(
+			(0..partial1_tests_num)
+				.map(|_| (0, 0, dist.sample(rng)))
+				.map(|i| (i, (i.2))),
+		);
+
+		// partial a+b
+		tests.extend(
+			(0..partial2_tests_num)
+				.map(|_| (dist.sample(rng), dist.sample(rng), 0))
+				.map(|input| (input, (input.0 + input.1))),
+		);
+
+		// partial b+c
+		tests.extend(
+			(0..partial2_tests_num)
+				.map(|_| (0, dist.sample(rng), dist.sample(rng)))
+				.map(|input| (input, (input.1 + input.2))),
+		);
+
+		// partial a+c
+		tests.extend(
+			(0..partial2_tests_num)
+				.map(|_| (dist.sample(rng), 0, dist.sample(rng)))
+				.map(|input| (input, (input.0 + input.2))),
+		);
+
+		// full
+		let num_left = num - tests.len();
+		tests.extend(
+			(0..num_left)
+				.map(|_| (dist.sample(rng), dist.sample(rng), dist.sample(rng)))
+				.map(|input| (input, (input.0 + input.1 + input.2))),
+		);
+
+		Self { tests }
+	}
+}
+
 impl Problem for Sum3 {
 	type In = (i32, i32, i32);
 	type Out = i32;
@@ -49,20 +113,10 @@ impl Problem for Sum3 {
 // 	}
 // }
 
-fn gen_tests(num: usize) -> Vec<((i32, i32, i32), i32)> {
-	let rng = &mut thread_rng();
-	let dist = Uniform::new(-256, 256);
-	(0..num)
-		.map(|_| (dist.sample(rng), dist.sample(rng), dist.sample(rng)))
-		.map(|arg| (arg, (arg.0 + arg.1 + arg.2)))
-		.collect()
-}
-
 fn main() {
 	pretty_env_logger::init();
 
-	let tests = gen_tests(1000);
-	let prob = Sum3 { tests };
+	let prob = Sum3::new(1000);
 	let seed: u64 = thread_rng().gen();
 	let muts: [&dyn Mutator<_, _>; 2] = [
 		// for use in sequence
@@ -82,14 +136,14 @@ fn main() {
 	};
 	let mut ga = WasmGenAlgBuilder::default()
 		.problem(prob)
-		.pop_size(250)
+		.pop_size(100)
 		.generations(100)
 		.stop_condition(Box::new(|ctx: &mut Context, _: &[WasmGenome]| -> bool {
 			ctx.max_fitness >= 1.0
 		}))
-		.selection(TournamentSelection::new(0.8, 2, 0.9, true)) // can do real tournament selection when selection is fixed
+		.selection(TournamentSelection::new(0.6, 3, 0.9, false)) // can do real tournament selection when selection is fixed
 		.enable_elitism(true)
-		.elitism_rate(0.02)
+		.elitism_rate(0.05)
 		.enable_crossover(true)
 		.crossover_rate(0.95)
 		// .crossover()
