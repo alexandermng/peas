687871bbdbc56402eaddddd8f152ae29cca18915
stefanse <stefan.stealey.euchner@gmail.com>
2024-09-16 11:56:14 -0700

    comments

--- src/wasm/species.rs
+++ src/wasm/species.rs
@@ -4,4 +4,7 @@ use std::{
 
 struct WasmSpecies {
     //TODO
+    //"list" or equivalent of the genomes in this species
+    //unique identifier?
+    //function to pick a random member of the species
 }
