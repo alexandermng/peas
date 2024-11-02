a20247cb1fb09948d244861d8882de69a2097a06
Alexander Ng <an@asu.edu>
2024-11-02 13:29:07 -0700

    starting definitions

--- src/params.rs
+++ src/params.rs
@@ -2,10 +2,10 @@ use serde::{Deserialize, Serialize};
 
 use crate::genetic::{AsContext, Genome, Mutator, OnceMutator, Predicate, Problem, Selector};
 
-/// Declare runtime parameters
-#[derive(Serialize, Deserialize)]
+/// Parameters for the program. Can be output to a file.
+#[derive(Serialize)]
 pub struct GenAlgParams {
-	pub seed: Option<u64>, // pre-set seed
+	pub seed: u64, // set seed for the run
 	pub pop_size: usize,
 	pub num_generations: usize,
 	pub mutation_rate: f64, // TODO consider
@@ -15,4 +15,23 @@ pub struct GenAlgParams {
 
 	#[serde(skip_serializing)]
 	pub log_file: Option<String>,
+}
+
+/// Input options to the
+#[derive(Deserialize)]
+pub struct GenAlgParamsOpts {
+	// TODO take GenAlgParams and... make everything optional. and String-like.
+}
+
+/// Input command-line arguments
+#[derive(clap::Parser, Debug)]
+pub struct GenAlgParamsCLI {
+	// TODO -F / --config [filename]
+	// TODO -s / --seed [seed]
+}
+
+impl From<GenAlgParamsOpts> for GenAlgParams {
+	fn from(value: GenAlgParamsOpts) -> Self {
+		todo!()
+	}
 }
